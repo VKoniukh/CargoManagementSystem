9fec15ed2086b09e582b60b2932c19d4aad78a44
Volodymyr <konuhvladimir1@gmail.com>
2021-09-17 17:19:09 +0300

    Create connection Spring security with Data base and related configuration was added

--- src/main/java/ua/koniukh/cargomanagementsystem/model/User.java
+++ src/main/java/ua/koniukh/cargomanagementsystem/model/User.java
@@ -1,23 +1,38 @@
 package ua.koniukh.cargomanagementsystem.model;
 
 import lombok.Data;
-
 import javax.persistence.*;
+import java.util.Set;
 
 @Data
 @Entity
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column(name= "user_name")
+    private String username;
+
+    @Column(name= "password")
+    private String password;
+
+    @Column(name= "email")
+    private String email;
+
+    @Column(name= "active")
+    private boolean active;
+
+    @Column(name= "roles")
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
 
     @Column(name= "first_name")
     private String firstname;
+
     @Column(name= "last_name")
     private String lastname;
-    @Column(name= "email")
-    private String email;
-    @Column(name= "password")
-    private String password;
 }
